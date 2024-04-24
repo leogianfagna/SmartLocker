2cf14dc2d4b668181e56bcb656551d34043f4212
Nina Canto <ninacanto25@gmail.com>
2024-04-24 17:16:44 -0300

    Finalizaçao da activity de pagamento
    Correçao de bugs e pequenos detalhes no texto de cada tela.

--- app/src/main/java/com/projetointegrador/smartlocker/ChangeActivity.kt
+++ app/src/main/java/com/projetointegrador/smartlocker/ChangeActivity.kt
@@ -1,20 +1,43 @@
 package com.projetointegrador.smartlocker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.FirebaseFirestore
+import com.projetointegrador.smartlocker.databinding.ActivityChangeBinding
 
 class ChangeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChangeBinding
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_change)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityChangeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCardConfirm.setOnClickListener {
+            val cardNumber = binding.etCardNumber.text.toString()
+            val cardCpf = binding.etCardCpf.text.toString()
+            val cardCvv = binding.etCardCvv.text.toString()
+            val cardValidity = binding.etCardExpirity.text.toString()
+            val cardName = binding.etCardHolder.text.toString()
+
+            val card = hashMapOf(
+                "cardNumber" to cardNumber,
+                "cardCpf" to cardCpf,
+                "cardCvv" to cardCvv,
+                "cardValidity" to cardValidity,
+                "cardName" to cardName
+            )
+
+            db.collection("cards")
+                .add(card)
+                .addOnSuccessListener { documentReference ->
+                    Toast.makeText(this, "Card registered successfully", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(this, "Error adding card", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 }
