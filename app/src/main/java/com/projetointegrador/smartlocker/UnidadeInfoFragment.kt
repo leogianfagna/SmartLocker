6030abe585873c69bad44caae29769796d6bf691
nicholastomic <nicholastomic@gmail.com>
2024-04-26 22:52:53 -0300

    Check para ver se o usuário tem cartão

--- app/src/main/java/com/projetointegrador/smartlocker/UnidadeInfoFragment.kt
+++ app/src/main/java/com/projetointegrador/smartlocker/UnidadeInfoFragment.kt
@@ -1,21 +1,27 @@
 package com.projetointegrador.smartlocker
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.room.util.findColumnIndexBySuffix
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.projetointegrador.smartlocker.databinding.FragmentUnidadeInfoBinding
+import kotlinx.coroutines.tasks.await
 
 class UnidadeInfoFragment : Fragment() {
     val bundle = Bundle()
 
+    private var statusCartao : Boolean = false
     private lateinit var firestore: FirebaseFirestore
     private var _binding: FragmentUnidadeInfoBinding? = null
     private val binding get() = _binding!!
+    private val auth = FirebaseAuth.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,24 +51,51 @@ class UnidadeInfoFragment : Fragment() {
 
         binding.btnBeginRent.setOnClickListener {
             // Todo: COLOCAR ALGUM CHECK PRA VER SE O USUÁRIO JA TEM UM ALUGUEL
-            //Iniciar activity renlocker
-            activity?.let{
-                //armazenar nome da unidade no bundle
-                bundle.putString("uni", nomeUnidade)
 
-                val intent = Intent(it, RentLockerActivity::class.java)
-                intent.putExtras(bundle)
-                it.startActivity(intent)
-            }
+            possuiCartaoCadastrado(nomeUnidade)
+
+
         }
 
         binding.btnVoltar.setOnClickListener {
             requireActivity().supportFragmentManager.beginTransaction().remove(this).commit()
         }
     }
+    private fun possuiCartaoCadastrado( nomeUnidade: String){
+
+        val idDoUsuarioAutenticado = FirebaseAuth.getInstance().currentUser!!.uid
+        Log.d(TAG, "successo $idDoUsuarioAutenticado")
+
+        firestore.collection("usuarios").document(idDoUsuarioAutenticado)
+            .get().addOnSuccessListener { document ->
+                statusCartao = document.getBoolean("cartao")!!
+                Log.d(TAG, "successo $statusCartao dentro do successo")
+                if (statusCartao==true){
+                    //Iniciar activity rentlocker
+                    activity?.let{
+                        //armazenar nome da unidade no bundle
+                        bundle.putString("uni", nomeUnidade)
+
+                        val intent = Intent(it, RentLockerActivity::class.java)
+                        intent.putExtras(bundle)
+                        it.startActivity(intent)
+                    }
+                }else{
+                    activity?.let{
+                        val intent = Intent(it, ChangeActivity::class.java)
+                        it.startActivity(intent)
+                    }
+                }
+            }.addOnFailureListener {
+                Log.d(TAG, "erro ao pegar os dados", it)
+            }
+
+
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
