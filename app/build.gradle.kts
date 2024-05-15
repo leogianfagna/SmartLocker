074a94d336b7607ad76df6902452c6674fcbb6ef
nicholastomic <nicholastomic@gmail.com>
2024-05-15 13:11:10 -0300

    Atualização da api de "com.journeyapps:zxing-android-embedded"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
 
     implementation ("com.google.code.gson:gson:2.10.1")
     implementation("com.google.zxing:core:3.4.1")
-    implementation("com.journeyapps:zxing-android-embedded:4.2.0")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation("com.google.android.gms:play-services-maps:18.1.0")
 
 }
