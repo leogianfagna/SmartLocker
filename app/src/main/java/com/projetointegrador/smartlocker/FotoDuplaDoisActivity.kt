7cdd55dd707913d47cad7a52fdba758d1d2da341
Nina Canto <ninacanto25@gmail.com>
2024-05-22 18:03:28 -0300

    Adição de novas activities

---
+++ app/src/main/java/com/projetointegrador/smartlocker/FotoDuplaDoisActivity.kt
@@ -0,0 +1,20 @@
+package com.projetointegrador.smartlocker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FotoDuplaDoisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_foto_dupla_dois)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
