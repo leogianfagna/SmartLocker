cf45ef79a19d254ad1fb86fb63cff81414c11ab8
Nina Canto <ninacanto25@gmail.com>
2024-05-23 14:48:59 -0300

    Novas funcionalidades adicionadas
    Agora os a maioria dos botões funcionam!

--- app/src/main/java/com/projetointegrador/smartlocker/FotoDuplaDoisActivity.kt
+++ app/src/main/java/com/projetointegrador/smartlocker/FotoDuplaDoisActivity.kt
@@ -1,20 +1,39 @@
 package com.projetointegrador.smartlocker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.projetointegrador.smartlocker.databinding.ActivityFotoDuplaDoisBinding
 
 class FotoDuplaDoisActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFotoDuplaDoisBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_foto_dupla_dois)
+        binding = ActivityFotoDuplaDoisBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_foto_dupla_um)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnContinue.setOnClickListener {
+            val activity = Intent(this, /*LINKAR OUTRA ACTIVITY AQUI*/ ::class.java)
+            startActivity(activity)
+            finish()
+        }
+
+        //BOTAO ABAIXO DESCARTA A FOTO TIRADA E ABRE A CAMERA NOVAMENTE
+
+        binding.btnOutraFoto.setOnClickListener {
+            val activity = Intent(this, CamPreviewActivity::class.java)
+            startActivity(activity)
+            finish()
         }
     }
 }
--- app/src/main/java/com/projetointegrador/smartlocker/FotoDuplaUmActivity.kt
+++ app/src/main/java/com/projetointegrador/smartlocker/FotoDuplaUmActivity.kt
@@ -1,20 +1,41 @@
 package com.projetointegrador.smartlocker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.projetointegrador.smartlocker.databinding.ActivityFotoDuplaUmBinding
 
 class FotoDuplaUmActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFotoDuplaUmBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        binding = ActivityFotoDuplaUmBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_foto_dupla_um)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //BOTAO ABAIXO ABRE A CAMERA NOVAMENTE E TIRA A FOTO DE OUTRO CLIENTE
+
+        binding.btnContinue.setOnClickListener {
+            val activity = Intent(this, CamPreviewActivity::class.java)
+            startActivity(activity)
+            finish()
+        }
+
+        //BOTAO ABAIXO DESCARTA A FOTO TIRADA E ABRE A CAMERA NOVAMENTE
+
+        binding.btnOutraFoto.setOnClickListener {
+            val activity = Intent(this, CamPreviewActivity::class.java)
+            startActivity(activity)
+            finish()
         }
     }
 }
